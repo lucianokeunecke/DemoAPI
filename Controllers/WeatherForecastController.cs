1bf54318071416047ee5b1b6f1bd52c21a9058b9
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2024-07-27 12:18:44 -0300

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Alunos")]
+        public Models.Aluno GetAlunos()
+        {
+            return new Models.Aluno
+            {
+                Id = 1,
+                Nome = "Luciano",
+                Email = "keunecke@gmail.com",
+                DataCadastro = DateTime.Now,
+                Ativo = true                                                    
+            };
+        }
+
+    }
+}
